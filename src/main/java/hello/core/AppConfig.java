c044e198af420d46c94bb793d1612f83c908bd07
yikangil <comportone90@naver.com>
2023-07-05 23:00:52 +0900

    23.07.05 work in spring boot

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -2,6 +2,7 @@ package hello.core;
 
 import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
@@ -13,17 +14,15 @@ public class AppConfig {
 	public MemberService memberService() {
 		return new MemberServiceImpl(memberRepository());
 	}
-
 	public OrderService orderService() {
 		return new OrderServiceImpl(memberRepository(), discountPolicy());
 	}
-
 	private MemoryMemberRepository memberRepository() {
 		return new MemoryMemberRepository();
 	}
-
 	public DiscountPolicy discountPolicy() {
-		return new FixDiscountPolicy();
+		//return new FixDiscountPolicy();
+		return new RateDiscountPolicy();
 	}
 
 }
