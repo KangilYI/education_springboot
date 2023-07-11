102e8092c0f3ec25010a414cfc4d65f78dd3f0ff
callisto <comportone90@naver.com>
2023-07-11 17:24:03 +0900

    23.07.11 inflean spring 기초강의 의존관계 자동주입 - lombok - @Qualifier, @primary - 애노테이션 직접 만들어적용 - 조회한 Bean을 모두 사용해야 할때 Bean 생명주기 콜백 - initializingbean, DisposableBean - @PostConstruct, @PreDestory

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -1,17 +1,18 @@
 package hello.core.order;
 
+import hello.core.annotation.MainDiscountPolicy;
 import hello.core.discount.DiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
-import hello.core.member.MemoryMemberRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
+
 public class OrderServiceImpl implements OrderService {
 
-	private MemberRepository memberRepository;
-	private DiscountPolicy discountPolicy;
+	private final MemberRepository memberRepository;
+	private final DiscountPolicy discountPolicy;
 
 	/**
 	 * @Autowired의 기본동작은 주입할 대상이 없으면 오류가 발생한다.
@@ -19,13 +20,8 @@ public class OrderServiceImpl implements OrderService {
 	 * 생성자가 하나일경우 스프링 컨테이너에서 자동으로 Autowired를 지정한다.
 	 */
 
-	public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
-		this.memberRepository = memberRepository;
-		this.discountPolicy = discountPolicy;
-	}
-
 	@Autowired
-	public void init(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+	public OrderServiceImpl(MemberRepository memberRepository, @MainDiscountPolicy DiscountPolicy discountPolicy) {
 		this.memberRepository = memberRepository;
 		this.discountPolicy = discountPolicy;
 	}
